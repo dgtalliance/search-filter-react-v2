ceff6faa07fac7910bd140c82a502a9168934ff5
Ing. Rosdanys Ramirez Arcas <rosdanys@dgtalliance.com>
2022-04-28 19:03:10 +0300

    fix files updates

--- src/config/store/index.jsx
+++ src/config/store/index.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 //reducers
 import properties from '../slices/properties';
 import propertiesDetails from '../slices/propertiesDetails';
+import autocomplete from '../slices/propertiesAutoComplete';
 
 export const store = configureStore({
   reducer: {
     properties,
-    propertiesDetails
+    propertiesDetails,
+    autocomplete
   },
 });
 
